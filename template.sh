4bc23d5ae0374c75459f7451529b2c0cecc482f2
Omkar Ravani <omkarrvani@gmail.com>
2025-11-24 15:17:51 +0530

    folder added

---
+++ template.sh
@@ -0,0 +1,15 @@
+ mkdir -p src
+ mkdir -p research
+
+
+ touch src/_init_.py
+ touch src/helper.py
+ touch src/prompt.py
+ touch .env
+ touch setup.py
+ touch app.py
+ touch research/trails.ipynb
+ touch requirment.txt
+
+
+ echo " created"
